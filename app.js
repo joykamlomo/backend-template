ed15869e68423c1b40265879cec6283c3538487e
joykamlomo <kamlomo.js@gmail.com>
2023-05-06 23:23:10 +0200

    changed file naming

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
 
 // Load environment variables from .env file
 dotenv.config();
 
 // Connect to the database
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to the database'))
-  .catch(err => console.error(err));
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.error(err));
 
 // Create a new Express app
 const app = express();
@@ -18,7 +22,9 @@ const app = express();
 app.use(express.json());
 
 // Mount the userRoutes router at the /users path
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 // Start the server
-app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  console.log(`Server started on port ${process.env.PORT}`)
+);
