ccf589d4253c50c9fbb3f72a8dc2d30193f8c16e
joykamlomo <kamlomo.js@gmail.com>
2023-05-06 23:18:54 +0200

    update user controller, added jwt and bcryptjs

--- controllers/user.js
+++ controllers/user.js
@@ -1,116 +1,58 @@
-// controllers/userController.js
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-// Import the User model
-const User = require("../models/user");
-
-// Define the controller methods
-exports.createUser = async (req, res) => {
+exports.signup = async (req, res) => {
   try {
+    // Check if user already exists with the same email address
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, message: 'User already exists' });
+    }
+
+    // Hash the password before storing in the database
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create a new user object using the request body
-    const user = new User(req.body);
+    const user = new User({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      password: hashedPassword
+    });
 
     // Save the user object to the database
     await user.save();
 
-    // Send a response indicating that the user was successfully created
-    res.status(201).json({
-      success: true,
-      message: "User created successfully",
-      user: user,
-    });
+    // Generate and send a JSON web token in the response for further authentication
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.status(201).json({ success: true, message: 'User created successfully', token });
   } catch (err) {
     // If an error occurs, send a response indicating that the user could not be created
-    res.status(500).json({
-      success: false,
-      message: "Failed to create user",
-      error: err.message,
-    });
+    res.status(500).json({ success: false, message: 'Failed to create user', error: err.message });
   }
 };
 
-exports.getUserById = async (req, res) => {
+exports.login = async (req, res) => {
   try {
-    // Retrieve the user with the specified ID from the database
-    const user = await User.findById(req.params.id);
-
-    // If the user is found, send a response with the user object
-    if (user) {
-      res.status(200).json({
-        success: true,
-        message: "User retrieved successfully",
-        user: user,
-      });
-    } else {
-      // If the user is not found, send a response indicating that no user was found with the specified ID
-      res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
+    // Check if user exists with the given email address
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(400).json({ success: false, message: 'Invalid email or password' });
     }
-  } catch (err) {
-    // If an error occurs, send a response indicating that the user could not be retrieved
-    res.status(500).json({
-      success: false,
-      message: "Failed to retrieve user",
-      error: err.message,
-    });
-  }
-};
 
-exports.updateUser = async (req, res) => {
-  try {
-    // Find the user with the specified ID and update their details using the request body
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-
-    // If the user is found and updated, send a response with the updated user object
-    if (user) {
-      res.status(200).json({
-        success: true,
-        message: "User updated successfully",
-        user: user,
-      });
-    } else {
-      // If the user is not found, send a response indicating that no user was found with the specified ID
-      res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
+    // Check if the password is valid using bcrypt
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword) {
+      return res.status(400).json({ success: false, message: 'Invalid email or password' });
     }
-  } catch (err) {
-    // If an error occurs, send a response indicating that the user could not be updated
-    res.status(500).json({
-      success: false,
-      message: "Failed to update user",
-      error: err.message,
-    });
-  }
-};
 
-exports.deleteUser = async (req, res) => {
-  try {
-    // Find the user with the specified ID and remove them from the database
-    const user = await User.findByIdAndRemove(req.params.id);
-
-    // If the user is found and removed, send a response indicating that the user was deleted
-    if (user) {
-      res.status(200).json({
-        success: true,
-        message: "User deleted successfully",
-      });
-    } else {
-      // If the user is not found, send a response indicating that no user was found with the specified ID
-      res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
+    // Generate and send a JSON web token in the response for further authentication
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.status(200).json({ success: true, message: 'Logged in successfully', token });
   } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Failed to delete user",
-      error: err.message,
-    });
+    // If an error occurs, send a response indicating that the user could not be logged in
+    res.status(500).json({ success: false, message: 'Failed to login', error: err.message });
   }
 };
