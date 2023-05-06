4711275be326a40505f7ca0c33458af0eeee8d87
joykamlomo <kamlomo.js@gmail.com>
2023-05-07 00:37:31 +0200

    fixed compile User model more than once

--- models/user.js
+++ models/user.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+// check if model has already been compiled
+const User = mongoose.models.User
+  ? mongoose.model("User")
+  : mongoose.model("User", new mongoose.Schema({
+      firstname: {
+        type: String,
+        required: true,
+      },
+      lastname: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+    }));
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = User;
